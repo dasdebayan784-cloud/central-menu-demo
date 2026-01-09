2155881cd0acc67644203a814ece97d15d02c02e
dasdebayan784-cloud <dasdebayan784@gmail.com>
2026-01-09 12:43:57 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-fetch("menu.json")
-  .then(res => res.json())
-  .then(data => {
-    const list = document.getElementById("menu");
-    data.menu.forEach(item => {
-      list.innerHTML += `<li>${item.name} - ${item.price}</li>`;
-    });
-  });
